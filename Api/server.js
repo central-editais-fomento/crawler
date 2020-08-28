88be307272ea6c383bbd7566e710e3bc648adbba
Vinícius Alves Hax <viniciushax@gmail.com>
2020-08-28 14:11:01 -0300

    Colocando parâmetros do MongoDB em um arquivo

--- Api/server.js
+++ Api/server.js
@@ -2,13 +2,15 @@ const express = require ('./node_modules/express');
 const cors = require ('./node_modules/cors/lib');
 const mongoose = require ('mongoose');
 const requireDir = require('./node_modules/require-dir');
+const { mongo } = require('../app.config');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://dbuser:dbuser@cluster0-jivi5.mongodb.net/test?retryWrites=true&w=majority', 
+connection_string = 'mongodb+srv://'+mongo.user+':'+mongo.password+'@'+mongo.url+'/'+mongo.database+'?'+mongo.options;
+mongoose.connect(connection_string, 
     { useNewUrlParser: true }
 )
 
